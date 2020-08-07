973e6ca134c7581a4104eda4fd240bcbd17260fe
Pavan Kumar Polavarapu <Pavan_Kumar.polavara@walmart.com>
2020-08-06 20:05:56 -0500

    001 - Springboot Template

---
+++ src/test/java/com/walmart/treasury/ExchangeRate/ExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.treasury.ExchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
